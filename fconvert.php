7345d7cf881608a7bf3d2e616a989f106da3dc12
chattyza <chattyza@gmail.com>
2020-03-10 14:56:18 +0700

    Update fconvert.php

--- fconvert.php
+++ fconvert.php
@@ -1,18 +1,10 @@
-﻿<html>
+<html>
 <title>แปลงอุณหภูมิจากฟาเรนไฮต์เป็นเซลเซียส</title>
 <body>
 
 <h2>แปลงอุณหภูมิ</h2>
 
-<p>
-  <label>ฟาเรนไฮต์</label>
-  <input id="Fahrenheit" type="number" placeholder="ฟาเรนไฮต์" oninput="temperatureConverter(this.value)" onchange="temperatureConverter(this.value)">
-</p>
-<p>Celcius: <span id="Celcius"></span></p>
 
-  <input id="Kelvin" type="number" placeholder="kelvin" oninput="Kelvinc(this.value)" onchange="Kelvinc(this.value)">
-
-<p>Kelvin: <span id="Kelvin"></span></p>
 <!-- Man -->
 <label>Input Celcius</label>
 <input id="inpC" type="text" oninput="Cto(this.value)" onchange="Cto(this.value)">
@@ -20,17 +12,7 @@
 <p>C to K: <span id="ctok"></span></p>
 <p>C to R: <span id="ctor"></span></p>
 
-<p>Kelvin: <span id="Kel"></span></p>
-
-<p>
-  <label>โรเมอร์</label>
-  <input id="Romur" type="number" placeholder="โรเมอร์" oninput="Romurc(this.value)" onchange="Romurc(this.value)">
-</p>
-<p>Romur: <span id="Rom"></span></p>
-
-
 <script>
-
 function temperatureConverter(num) {
   num = parseFloat(num);
   document.getElementById("Celcius").innerHTML=(num-32)/1.8;
@@ -39,7 +21,6 @@ function Kelvinc(num) {
   num = parseFloat(num);
   document.getElementById("Kel").innerHTML=num + 273;
 }
-
 //Man
 function Cto(num){
   c = parseFloat(num);
@@ -51,8 +32,6 @@ function Romurc(num) {
   num = parseFloat(num);
   document.getElementById("Rom").innerHTML=num*0.8;
 }
-
 </script>
-
 </body>
 </html>
