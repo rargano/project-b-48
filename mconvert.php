f7ed51380b25b23439c5845a364cdf74972893a8
MRrargano <rargano@gmail.com>
2020-03-11 09:45:14 +0700

    add Yuan

--- mconvert.php
+++ mconvert.php
@@ -38,5 +38,15 @@ function Cto(num){
   document.getElementById("ctof").innerHTML = c*0.32;
 }
 </script>
+<h3>Thai Bath To Yuan</h3>
+<label>Thai Bath</label>
+<input id="thb" oninput="ConvertToYuan(this.value)" onchange="ConvertToYuan(this.value)">
+<label>= Yuan: </label><span id="yuan"></span>
+
+<script>
+function ConvertToYuan(num){
+    var str = 'Yuan';
+    document.getElementById("yuan").innerHTML = num * 0.22;
+}
 </body>
 </html>
